39db04feecbb02865257a8c03d07151ab6783a6b
luckyWJQ <11825150@qq.com>
2017-11-23 21:01:28 +0800

    first commit

---
+++ helloaaaaaaa/src/com/edu/sdu/Test1.java
@@ -0,0 +1,14 @@
+package com.edu.sdu;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}

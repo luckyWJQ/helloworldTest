1516a4c3fd3d98c86dde56575a2fda4cc1a58211
luckyWJQ <11825150@qq.com>
2017-11-23 21:03:18 +0800

    第2次提交

--- helloaaaaaaa/src/com/edu/sdu/Test2.java
+++ helloaaaaaaa/src/com/edu/sdu/Test2.java
@@ -1,14 +1,13 @@
 package com.edu.sdu;
 
-public class Test1 {
+public class Test2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		System.out.println("aa");
+		System.out.println("第2个");
 	}
 
 }
